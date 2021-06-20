32004ab2ae8e85c81fb4193dbaf283b58679ae21
Devon <devonross50@yahoo.com>
2021-06-20 15:03:05 -0500

    updated post tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -13,33 +13,40 @@ router.get('/', (req, res) => {
         attributes: ['product_name','price','stock']
       }
     ]
-  });
+  }).then((getAllTags) => {
+    res.json(getAllTags);
+});
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   Tag.findAll({
-    where: {id:rreq.params.id},
+    where: {id:req.params.id},
     include: [
       {
         model: Product,
         attributes: ['product_name','price','stock']
       }
     ]
-  })
+  }).then((getTagID) => {
+    res.json(getTagID);
+});
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
-  Tag.update({
-    where: {id:req.params.id}
+router.post('/postTest', (req, res) => {
+
+  Tag.create({
+    tag_name: req.body.tag_name
+  }).then((postTags) => {
+    res.json(postTags);
   })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
   Tag.update({
+    tag_name: req.body.tag_name,
     where: {id:req.params.id}
   })
 });
